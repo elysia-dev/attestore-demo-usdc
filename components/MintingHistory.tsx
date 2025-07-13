eea91526714c6d70e07ed353bcd8d654257543ea
hyunoh-modori <hyunoh.bae@modori.io>
2025-07-13 14:05:06 +0900

    Fix build issue

--- components/MintingHistory.tsx
+++ components/MintingHistory.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useCallback, useEffect, useState } from "react";
 import { useAccount, usePublicClient } from "wagmi";
 import { formatUnits } from "viem";
