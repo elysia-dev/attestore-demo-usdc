f75db13c002bc6f9e98266adb72ab0cb8590d3db
qpzm <lhm1442@gmail.com>
2025-07-13 14:16:11 +0900

    Fix Module not found: Can't resolve 'pino-pretty

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'encoding');
+    return config;
+  },
 };
 
 const project =
