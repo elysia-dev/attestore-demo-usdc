8bc29615b843a2e3ca9c72610dacad8757f9e56c
kyngwan@gmail.com <kyngwan@gmail.com>
2025-07-24 14:21:10 +0900

    Install lint-staged, prettier, husky

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// .prettierrc.js
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: "all",
+  semi: false,
+  // Override any other rules you want
+};
