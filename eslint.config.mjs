62e818b56793c2c2c6cf9bd41b454504747c8442
kyngwan@gmail.com <kyngwan@gmail.com>
2025-07-25 13:24:10 +0900

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const config = [
     rules: {
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
   {
